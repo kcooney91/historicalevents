d7f5e3cc5acc7cf30cb29c1c5fe873577810a458
Karl Cooney <karl.cooney@hmhco.com>
2022-06-03 15:56:07 +0100

    removing limit from API

--- src/components/Functional/fetchAPI.js
+++ src/components/Functional/fetchAPI.js
@@ -37,7 +37,7 @@ const fetchAPI = async (callback) => {
   let result;
   try {
     fetch(
-      "https://www.vizgr.org/historical-events/search.php?format=json&begin_date=-3000000&end_date=20151231&lang=en&limit=100"
+      "https://www.vizgr.org/historical-events/search.php?format=json&begin_date=-3000000&end_date=20151231&lang=en"
     ).then((response) => {
       processChunkedResponse(response, callback);
     })
